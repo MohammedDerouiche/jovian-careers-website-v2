40c28700f2e306510888c2aa235ec6239a7dd733
Mohammed Derouiche <mohammed.der.work@gmail.com>
2023-09-28 10:45:08 +0100

    the first app

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-print('hello, world')
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_hello():
+    return "hello, world from mohammed der"
+
+if __name__ != "main":
+    app.run(host="0.0.0.0", debug=True)
