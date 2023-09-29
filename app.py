71d11c37f9ddbcd325c3fb845b17f0f951b67b6e
Mohammed Derouiche <mohammed.der.work@gmail.com>
2023-09-29 14:59:43 +0100

    Adding html file with css styles

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_hello():
-    return "hello, world from mohammed der"
+    return render_template("home.html")
 
-if __name__ != "main":
-    app.run(host="0.0.0.0", debug=True)
+# if __name__ != "main":
+#     app.run(host="0.0.0.0", debug=True)
