3a1948690f3280bdfba68f4b70768a209cfd40f6
Mohammed Derouiche <mohammed.der.work@gmail.com>
2023-10-03 20:35:50 +0100

    Import jobs from local mysqlDB rather then py dict

--- app.py
+++ app.py
@@ -1,41 +1,17 @@
 from flask import Flask, render_template, jsonify
+from database import load_jobs_from_db
 
 app = Flask(__name__)
 
-JOBS = [
-    {
-        'id' : 1,
-        "title" : "Data Scintist",
-        "location" : "Algeria, alger",
-        'salary' : 'RS. 10,10,100'
-    },
-    {
-        'id' : 2,
-        "title" : "Data Analyst",
-        "location" : "Algeria, el-oued",
-        'salary' : 'RS. 10,10,100'
-    },
-    {
-        'id' : 3,
-        "title" : "Front-End Engineer",
-        "location" : "Egybt, alexandia",
-        'salary' : 'RS. 12,10,100'
-    },
-    {
-        'id' : 4,
-        "title" : "Back-End Engineer",
-        "location" : "Tunisia, Sousa",
-        'salary' : 'RS. 20,10,100'
-    }
-]
-
 @app.route("/")
 def hello_hello():
-    return render_template("home.html", jobs=JOBS)
+    jobs = load_jobs_from_db()
+    return render_template("home.html", jobs=jobs)
 
-@app.route("/jobs")
+@app.route("/api/jobs")
 def list_jobs():
-    return jsonify(JOBS)
+    jobs = load_jobs_from_db()
+    return jsonify(jobs)
 
 # if __name__ != "main":
 #     app.run(debug=True)
