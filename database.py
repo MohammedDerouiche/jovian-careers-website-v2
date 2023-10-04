a1d57c66ac8d399228d60ddf4dd0a76cf0615e61
Mohammed Derouiche <mohammed.der.work@gmail.com>
2023-10-04 21:09:50 +0100

    dynamic pages for each job

--- database.py
+++ database.py
@@ -14,4 +14,14 @@ def load_jobs_from_db():
         
         return jobsList
 
+def load_job_from_db(id):
+    with engine.connect() as conn:
+        result = conn.execute(text(f"select * from jobs where id = {id}"))
+
+        job = result.fetchall()
+        if len(job) ==0:
+            return None
+        else:
+            return job[0]._asdict()
+
     
