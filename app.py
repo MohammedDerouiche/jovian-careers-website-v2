8b23c7630200fb47858d761fdb43d722bada9602
Mohammed Derouiche <mohammed.der.work@gmail.com>
2023-10-05 23:05:27 +0100

    add api for each job

--- app.py
+++ app.py
@@ -8,10 +8,10 @@ def hello_hello():
     jobs = load_jobs_from_db()
     return render_template("home.html", jobs=jobs)
 
-@app.route("/api/jobs")
-def list_jobs():
-    jobs = load_jobs_from_db()
-    return jsonify(jobs)
+@app.route("/api/jobs/<id>")
+def show_job_json(id):
+    job = load_job_from_db(id)
+    return jsonify(job)
 
 @app.route("/job/<id>")
 def show_job(id):
